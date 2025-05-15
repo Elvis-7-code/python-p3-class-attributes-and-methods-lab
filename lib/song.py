cbe5ad929098924047a82efe01600816df426fa0
Elvis <elvis.wachira@student.moringaschool.com>
2025-05-15 13:01:40 +0300

    add_to_

--- lib/song.py
+++ lib/song.py
@@ -8,7 +8,9 @@ class Song:
         Song.genres.append(genre)
         Song.artist.append(artist)
 
-        # Song.add_to_genres()
-        # Song.add_to_artists()
+        Song.add_to_genres()
+        Song.add_to_artists()
+        Song.add_to_genre_count(genre)
+        Song.add_to_artist_count(artist)
 
     
