9c4e0fc318a2747387791a6e69866a42a0733613
Elvis <elvis.wachira@student.moringaschool.com>
2025-05-15 12:55:44 +0300

    song.py-class changes

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,14 @@
 class Song:
-    pass
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        # Song.add_song_tocount()
+        # Song.genres.append(genre)
+        # Song.artist.append(artist)
+
+        # Song.add_to_genres()
+        # Song.add_to_artists()
+
+    
